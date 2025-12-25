c5cda78a5212b0b9bbebbafba314c93a43dd09bf
GitHub gen-configs Action <action@github.com>
2025-12-25 23:42:27 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_f429-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f429-discovery.h
@@ -2106,7 +2106,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.12.20.f429-discovery.356625080"
+#define TS_SIGNATURE "rusEFI master.2025.12.25.f429-discovery.356625080"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
