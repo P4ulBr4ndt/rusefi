808a7ab231bfb72c5303173bd9ea7ee3e2fd489a
GitHub gen-configs Action <action@github.com>
2026-02-09 12:45:11 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_uaefi.h
+++ firmware/controllers/generated/rusefi_generated_uaefi.h
@@ -2100,7 +2100,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.06.uaefi.3667901736"
+#define TS_SIGNATURE "rusEFI master.2026.02.09.uaefi.3667901736"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
