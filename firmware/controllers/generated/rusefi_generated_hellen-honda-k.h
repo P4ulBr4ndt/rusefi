7559eeb68ad2abeb0ae3ca5063d7a025442fa8cc
GitHub gen-configs Action <action@github.com>
2026-08-06 14:16:06 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
+++ firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
@@ -1560,7 +1560,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 4215327401
+#define SIGNATURE_HASH 2996822431
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2163,7 +2163,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.07.16.hellen-honda-k.4215327401"
+#define TS_SIGNATURE "rusEFI master.2026.08.06.hellen-honda-k.2996822431"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
