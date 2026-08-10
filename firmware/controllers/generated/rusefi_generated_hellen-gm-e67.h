af7a7666c019a261b6c2e3c7eb6dead741cbd329
GitHub gen-configs Action <action@github.com>
2026-08-10 13:09:47 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen-gm-e67.h
+++ firmware/controllers/generated/rusefi_generated_hellen-gm-e67.h
@@ -1562,7 +1562,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3135503688
+#define SIGNATURE_HASH 466297873
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2162,7 +2162,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.08.06.hellen-gm-e67.3135503688"
+#define TS_SIGNATURE "rusEFI master.2026.08.10.hellen-gm-e67.466297873"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
